23499b257633d2515762fb6863c0308d486f7422
Ubuntu <ubuntu@ip-172-31-26-142.ec2.internal>
2022-03-02 22:32:40 +0000

    added

---
+++ src/stats.ts
@@ -0,0 +1,50 @@
+/*
+ * This is a program that calculates mean, median and mode
+ * after reading in a text file into an array.
+ * 
+ * @author  Abdul Basit Butt
+ * @version 1.0
+ * @since   2022-02-19
+ */
+
+// Typescript Statistics program
+const fs = require('fs')
+
+const folderName = '/home/ubuntu/ICS4U/Unit01/unit1-06/ics4u-unit1-06-typescript/src'
+try {
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName)
+  }
+} catch (err) {
+  console.error(err)
+}
+
+
+function mean(arrayOfIntegers: any) {
+    let size: number = arrayOfIntegers.length
+    var  total = 0
+    for (let singleElementinArray in arrayOfIntegers) {
+        total += parseFloat(singleElementinArray)
+    }
+    let meanValue: number = (total/size)
+    return meanValue
+}
+function median(arrayOfIntegers: any) {
+    var returnValue: number
+    var array = arrayOfIntegers
+    array.sort()
+    let numberOfNumbers = array.length
+    if (numberOfNumbers % 2 == 0){
+        let numberOfMedian1 = (numberOfNumbers/2) - 1
+        let median1:number = array[numberOfMedian1]
+        let numberOfMedian2 = (numberOfNumbers/2)
+        let median2: number = array[numberOfMedian2]
+        returnValue = (median1+median2) / 2
+    } else {
+        let numberOfMedian = (numberOfNumbers / 2) + 0.5
+        returnValue = (array[numberOfMedian])
+    }
+        return returnValue
+}
+
+console.log('\nCalculating stats...')
